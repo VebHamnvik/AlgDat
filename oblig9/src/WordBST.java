a8c8b820d3ea843f961ab83c2dccbc532eb408ec
Vebjørn Solberg Hamnvik <vebjorsh@hiof.no>
2024-02-29 13:44:33 +0100

    Ferdig oblig 9

---
+++ oblig9/src/WordBST.java
@@ -0,0 +1,107 @@
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
+
+import static java.lang.System.in;
+
+// WordBST: Binært søketre med ord og ordfrekvenser
+public class WordBST
+{
+
+    private TreeMap<String,Integer> tre; // Roten i hele søketreet
+
+
+    // WordBST(): Konstruktør som lager et tomt søketre
+    public WordBST()
+    {
+	tre = new TreeMap<>();
+    }
+
+    // size(): Antall ord som er lagret i treet
+    public int size()
+    {
+	    return tre.size();
+    }
+
+    // insert(): Setter inn ny forekomst av et ord
+    public void insert(String ord)
+    {
+        //Hvis ordet er i treet fra før så vil verdien øke med 1
+        if (tre.containsKey(ord)){
+            Integer n = tre.get(ord);
+            n++;
+            tre.replace(ord, n);
+        }
+        else {
+            tre.put(ord, 1);
+        }
+
+    }
+
+    // search(): Søk etter et ord. Skriv ut ordet og ordfrekvensen
+    // hvis det finnes i søketreet.
+    public void search(String ord)
+    {
+        if (tre.containsKey(ord))
+            System.out.println(ord + " appears " + tre.get(ord) + " times in the text.");
+    }
+    
+    // print(): Alfabetisk utskrift av hele søketreet. Kaller en
+    // rekursiv metode som gjør selve utskriften.
+    public void print()
+    {
+	print(tre);
+    }
+
+    // print(): Rekursiv utskrift av hele søketreet med rot i "rot"
+    private void print(TreeMap<String, Integer> tre)
+    {
+        for (Map.Entry<String, Integer> entry : tre.entrySet()){
+            System.out.println(entry.getKey() + " appears " + entry.getValue() + " times in the text");
+        }
+    }
+    
+    // main(): Testprogram
+    public static void main (String argv[])
+    {
+	// Leser filnavn fra bruker
+	Scanner scan = new Scanner(in);
+	System.out.print("File? ");
+	String fileName = scan.next();
+    String path = Paths.get("").toAbsolutePath().toString();
+
+	// Oppretter ordleser og tomt søketre
+    // kan være denne må endres på for å få den til å finne txt filen
+	WordReader wR = new WordReader(path+"/oblig9/data/"+fileName);
+	WordBST wBST = new WordBST();
+
+	// Leser alle ordene på filen og legger inn i treet
+	String ord = wR.nextWord();
+	while (ord != null)
+	{
+	    wBST.insert(ord);
+	    ord = wR.nextWord();
+	}
+	// Skriver ut antall ulike ord som fantes i filen
+	System.out.println(wBST.size() + " unique words " +
+			   "read from file " + fileName);
+
+	// Menyvalg for å teste programmet
+	int valg = 0;
+	while(valg != 3)
+	{
+	    System.out.print("\n1:Search, 2:Print, 3:Quit ? ");
+	    valg = scan.nextInt();
+	    if (valg == 1)
+	    {
+		System.out.print("Search for? ");
+		ord = scan.next();
+		wBST.search(ord.toLowerCase());
+	    }
+	    else if (valg == 2)
+		wBST.print();
+	}
+    }
+}
---
+++ oblig9/src/WordReader.java
@@ -0,0 +1,87 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+// WordReader: Read a text file word-by-word
+// Author: Jan Høiberg, 2024
+// Note: All non-alphabetic characters are ignored
+
+public class WordReader
+{
+    private BufferedReader reader; // Line-by-line file reader
+    private String S[];            // All words on last read line
+    private int numWords;          // Number of words on last line
+    private int currentWord;       // Next word to be delievered
+    private boolean finished;      // True if all words on file read
+
+    // WordReader(): Constructor, opens file for reading
+    public WordReader(String fileName)
+    {
+	currentWord = numWords = 0;
+	finished = false;
+	try {reader = new BufferedReader(new FileReader(fileName));}
+	catch (IOException e) {e.printStackTrace();}
+    }
+
+    // nextWord(): Returns next word on file, null if all words read
+    public String nextWord()
+    {
+	// Finished reading all words on file?
+	if (finished)
+	    return null;
+
+	// If there are no more words left on the last line read from
+	// file, then read a new line and split it into separate words
+	if (currentWord == numWords)
+	{
+	    String line = "";
+	    // Read new line from file, skip both blank/empty lines
+	    // and lines with no alphabetical characters
+	    while (line.length() == 0)
+	    {
+		// Try reading a line of text
+		try {line = reader.readLine();}
+		catch (IOException e) {e.printStackTrace();}
+		// No more words on file?
+		if (line == null)
+		{
+		    finished = true;
+		    return null;
+		}
+		// Replace non-alphabetic characters with single space
+		line = line.replaceAll("[^\\p{IsAlphabetic}]+", " ");
+		// Trim off leading and trailing whitespace
+		line = line.trim();
+	    }
+	    // Convert line to lowercase
+	    line = line.toLowerCase();
+	    // Split line into array of words
+	    S = line.split(" ");
+	    // Set number of words on line and the index of the next
+	    // word to be returned from the method
+	    numWords = S.length;
+	    currentWord = 0;
+	}
+	// Return next word from last read line
+	String word = S[currentWord];
+	currentWord++;
+	return word;
+    }
+
+    // main(): Test program, prints all words on a given file
+    public static void main (String argv[])
+    {
+	Scanner scan = new Scanner(System.in);
+	System.out.print("File? ");  
+	String fileName = scan.nextLine();    
+
+	WordReader wR = new WordReader(fileName);
+	String word = wR.nextWord();
+	while (word != null)
+	{
+	    System.out.println(word);
+	    word = wR.nextWord();
+	}
+    }
+}
